caa1cd3a607d5e8b33245fd399a1ca6ec533e534
Maggie Moreno <maggie@tubularlabs.com>
2015-06-28 11:17:45 -0700

    update email form to thwart annoying robots

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,16 @@
 class MessagesController < ApplicationController
   
   def new
-    message = { 
-      name: params[:name], 
-      email: params[:email], 
-      content: params[:content] 
+    robot_box = params[:robot]
+    message = {
+      name: params[:name],
+      email: params[:email],
+      content: params[:message]
     }
-    
-    if message.values.any?(&:blank?)
+
+    if robot_box
+      render json: "Robot alert!", status: 400
+    elsif message.values.any?(&:blank?)
       render json: "Content can't be blank.", status: 400
     else
       MyMailer.visitor_email(message).deliver
